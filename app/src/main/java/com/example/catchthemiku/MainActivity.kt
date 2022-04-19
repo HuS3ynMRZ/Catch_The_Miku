8ba5a7559e4cab84d3139d2f08051dea606eb500
Huseyn <90988414+HuS3ynMRZ@users.noreply.github.com>
2022-04-19 10:49:59 +0400

    Score incremental system was implemented and succesfully run

--- app/src/main/java/com/example/catchthemiku/MainActivity.kt
+++ app/src/main/java/com/example/catchthemiku/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.catchthemiku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun upScore(view:View){
+        score += 1
+        scoreText.text = "Score: $score"
     }
 }
